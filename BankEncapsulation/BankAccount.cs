999a8bf8fbbd58de9be2691a2ff92af859cd33e2
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-12-09 12:39:03 -0500

    Completed

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+
+        public double Deposit(double amount) 
+        { 
+            balance += amount;
+            return balance;
+        }
+
+        public double GetBalance() 
+        { 
+            return balance;
+        }
+
+        public double Withdraw(double amount) 
+        {
+            balance -= amount;
+            return balance;
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+           BankAccount userBank = new BankAccount(0);
+
+            Console.WriteLine("Hello User: How much money would you like to deposit into the account?");
+            double moneyIn = double.Parse(Console.ReadLine());
+
+
+            userBank.Deposit(moneyIn);
+            Console.WriteLine($"You have added ${moneyIn} to you account.");
+
+            Console.WriteLine("");
+
+            double addedBalance = userBank.GetBalance();
+
+            Console.WriteLine($"Your new total balance is ${addedBalance}.");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Would you like to make a withdrawl? (Y/N) ");
+            var response = Console.ReadLine();
+            if (response == "Y")
+            {
+                Console.WriteLine("How much money would you like to withdraw?");
+                double moneyOut = double.Parse(Console.ReadLine());
+                userBank.Withdraw(moneyOut);
+                Console.WriteLine($"You have removed ${moneyOut} from your account.");
+                Console.WriteLine();
+
+                double removedBalance = userBank.GetBalance();
+
+                Console.WriteLine($"Your new balance is ${removedBalance}: have a great day!");
+
+            }
+            else
+            { 
+                Console.WriteLine("Have a great day!"); 
+            }
+
+
+
         }
     }
 }
